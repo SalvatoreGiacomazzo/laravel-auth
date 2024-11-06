7ee11a4a99847db4408990018da1f749ad9cbd67
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-06 19:23:31 +0100

    added login validation

--- laravel-new-exercise/app/Http/Controllers/Auth/LoginController.php
+++ laravel-new-exercise/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,23 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    public function login(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->intended('/home');
+        }
+
+
+        return back()->withErrors([
+            'email' => 'Your credentials are not registered in our database, please try again.',
+            'password' => 'Your credentials are not registered in our database, please try again.'
+        ]);
     }
 }
