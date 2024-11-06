52127e5780e9f25a54a5246e33f6eae79adb31e0
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-06 18:00:01 +0100

    displayed table in page and added paginator

--- laravel-new-exercise/app/Http/Controllers/WantedController.php
+++ laravel-new-exercise/app/Http/Controllers/WantedController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wanted;
+
 use Illuminate\Http\Request;
 
 class WantedController extends Controller
 {
     public function index()
     {
-
-        return view('wanted.home');
+        $wantedList = Wanted::all();
+        $wantedList = Wanted::paginate(10);
+        return view('wanted.home', compact('wantedList'));
     }
 }
--- laravel-new-exercise/resources/views/wanted/home.blade.php
+++ laravel-new-exercise/resources/views/wanted/home.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.layout')
 
 @section('main-content')
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. A perferendis quia reiciendis ea odit ipsum illo repellendus dolorem perspiciatis facere dignissimos labore eveniet, minima ut culpa commodi, et quo odio.
+
+
+<table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Date of Birth</th>
+        <th scope="col">Nationality</th>
+        <th scope="col">Felony</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($wantedList as $wanted)
+        <tr>
+          <th scope="row">{{ $wanted->id }}</th>
+          <td>{{ $wanted->name }}</td>
+          <td>{{ $wanted->last_name }}</td>
+          <td>{{ $wanted->date_of_birth }}</td>
+          <td>{{ $wanted->nationality }}</td>
+          <td>{{ $wanted->felony }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+  {{$wantedList->links()}}
 @endsection
